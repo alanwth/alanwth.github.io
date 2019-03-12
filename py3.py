2e6525a981951d01f8a5d3b2212c3ff66ebbe0cc
alanwth <40289083+alanwth@users.noreply.github.com>
2019-03-12 22:02:08 +0000

    Create py3.py

---
+++ py3.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import pandas_datareader.data as web
+from matplotlib import style
+import matplotlib.pyplot as plt
+import os
+import datetime as dt
+import pickle
+
+def compile_data():
+    with open("ssa.pickle","rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+
+    for count,ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns = {'Adj Close' : ticker}, inplace=True)
+        df.drop(['Open','High','Low','Close','Volume'], 1, inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+
+    print(main_df.head())
+    main_df.to_csv('ssa_joined_closes.csv')
+
+compile_data()
