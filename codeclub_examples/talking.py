9e56c4d05109b48fa0de7af3bf4383fc705629d6
alanwth <40289083+alanwth@users.noreply.github.com>
2019-03-14 16:09:34 +0000

    Create talking.py

---
+++ codeclub_examples/talking.py
@@ -0,0 +1,13 @@
+'''This is an exercise where the students are required to get input from the user and print according messages out.'''
+
+#Promting for a name and printing the name
+name = input("What is your name?: ")
+'''For teh students to learn about how to print multiple strings in one print statement'''
+print("Hi " + name + "!")
+
+#Promting for an age and printing the age
+'''In which the students will also learn about hte differnt types of data tpye and how to change from one to the other'''
+age = int(input("How old are you?: "))
+ageinten = (age + 10)
+print("You will be " + str(ageinten) + " years old in 10 years!")
+
